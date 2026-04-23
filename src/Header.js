042e3cdcd9a6763625b270a4924e19ed58d54308
amsKane <kaneams17@gmail.com>
2026-04-23 01:30:23 +0000

    Setup initial SenTransport avec Header

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+const Header = () => 
+    <header className = "header" >
+        <h1 className = "header-titre"> SenTransport </h1>
+            <p className = "header-soustitre">Votre guide du transport en commun a Dakar</p>
+    </header>; 
+
+export default Header;
